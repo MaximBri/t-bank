8014958fcc9837be948bcd59e99a0a17b5aaefec
Maxim <MaximK38136@yandex.ru>
2026-04-11 14:04:48 +0400

    feat(front): настройка prettier, eslint, vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
     port: 5173,
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/testSetup.ts',
+    globals: true,
   },
 })
