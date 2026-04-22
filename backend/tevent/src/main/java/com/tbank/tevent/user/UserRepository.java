9991ca3e35060d9b3a0da1f7d043212dd02e95c8
Andrey Novikov <andreyka0505@gmail.com>
2026-04-22 20:06:34 +0400

    feat: add user entity and user repository

---
+++ backend/tevent/src/main/java/com/tbank/tevent/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tbank.tevent.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByLogin(String login);
+    boolean existsByLogin(String login);
+}
