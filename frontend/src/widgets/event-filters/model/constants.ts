e568a45828eb37780f4fea32b23dc7a09cbeffc3
Maxim <MaximK38136@yandex.ru>
2026-05-17 13:31:16 +0400

    fix: баг с фильтрами событий на главной

--- frontend/src/widgets/event-filters/model/constants.ts
+++ frontend/src/widgets/event-filters/model/constants.ts
@@ -1,6 +1,16 @@
+import { EventStatus } from '@/entities/event'
 import { EventFilterStatus, type EventFiltersState } from './types.ts'
 
 export const eventFiltersStorageKey = 'event-filters'
+
+export const filterStatusToEventStatus: Record<
+  Exclude<EventFilterStatus, EventFilterStatus.All>,
+  EventStatus
+> = {
+  [EventFilterStatus.Active]: EventStatus.Active,
+  [EventFilterStatus.Planned]: EventStatus.Planned,
+  [EventFilterStatus.Completed]: EventStatus.Completed,
+}
 
 export const initialEventFiltersState: EventFiltersState = {
   search: '',
